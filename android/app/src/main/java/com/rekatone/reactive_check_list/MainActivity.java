c23e5b0b0a763c2aca1fff36db6d3d14bdb0f34e
Mohamad Rais <46097877+RaisTMJ@users.noreply.github.com>
2020-10-27 16:38:32 +0800

    fix android checklist, update to using android x

---
+++ android/app/src/main/java/com/rekatone/reactive_check_list/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rekatone.reactive_todo_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter_windows_1.17.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\github\reactive_flutter_todo_database_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter_windows_1.17.5-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
